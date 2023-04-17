9607d70ceec0b970fbffd67c56caa35adccb59af
Pave1Pal <pd271828@gmail.com>
2023-04-17 16:16:15 +0200

    chore: delete empty issuer

--- backend/src/base/controller/routes/credential.route.ts
+++ backend/src/base/controller/routes/credential.route.ts
@@ -1,6 +1,5 @@
 import { CredentialRoutes, Route } from "../../types/route.js";
 import { ethereumAccountRoutes } from "../../../mates/ethereum/issuers/ethereum-account/index.js";
-import { emptyRoutes } from "../../../mates/etc/issuers/empty/index.js";
 import { twitterAccountRoutes } from "../../../mates/twitter/issuers/twitter-account/index.js";
 import { githubAccountRoutes } from "../../../mates/github/issuers/github-account/index.js";
 import { discordAccountRoutes } from "../../../mates/discord/issuers/discord-account/index.js";
@@ -8,7 +7,6 @@ import { verifyCredentialEP } from "@sybil-center/sdk/util";
 import { credentialSchema } from "../../schemas/credential.schema.js";
 
 export const genVCRotes: CredentialRoutes[] = [
-  emptyRoutes,
   ethereumAccountRoutes,
   twitterAccountRoutes,
   githubAccountRoutes,
