97e998e294523d2d81dca9cf0159e25abf8efa4b
Pave1Pal <pd271828@gmail.com>
2023-04-17 16:20:18 +0200

    create extract props func

--- backend/src/util/model.util.ts
+++ backend/src/util/model.util.ts
@@ -1,1 +1,15 @@
 export type AnyObj = { [key: string]: any }
+
+/**
+ * Extract properties from origin object to new object
+ * @param obj origin object
+ * @param props props of origin object which will be extracted to new object
+ */
+export function extractProps<T extends AnyObj = AnyObj>(obj: T, props?: string[]): Partial<T> {
+  if (!props) return {};
+  const keys = Object.keys(obj);
+  const newProps = keys.filter((key) => props.includes(key));
+  const newObj: AnyObj = {}
+  newProps.forEach((prop) => newObj[prop] = obj[prop]);
+  return newObj as Partial<T>
+}
