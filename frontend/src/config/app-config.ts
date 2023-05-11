e5c4e237469ff6f1047ff627077ad0fbec18534d
Pavel <92925215+Pave1Pal@users.noreply.github.com>
2023-05-12 00:56:38 +0300

    fix(frontend): REACT_APP_VC_ISSUER_DOMAIN (#23)
    * update docs (#20)
    
    * chore(sybil): delete credential-provider type
    
    * docs(sybil): update documentation
    
    * chore(sybil): ancillary
    
    * refactor(demo-app): do refactor
    
    * refactor(frontend): rename proof provider hook variable
    
    * docs(sybil): verify credential section
    
    * chore(frontend): change sybil instance
    
    * chore(backend): future comment
    
    * docs(sybli): updadte docs
    
    * chore(sybil & backend): little improvement  (#21)
    
    * chore(backend): ancillary
    
    * chore(backend): merge
    
    * docs(sybil): little doc improvement
    
    * chore(sybil): to version 1.0.0
    
    * fix(frontend): throw if VC_ISSUER_DOMAIN is undefined

--- frontend/src/config/app-config.ts
+++ frontend/src/config/app-config.ts
@@ -3,7 +3,7 @@ export const appConfig = {
   infuraApiKey: getStrOrThrow("INFURA_API_KEY"),
   walletConnectProjectId: getStrOrThrow("WALLET_CONNECT_PROJECT_ID"),
   walletAppName: getStrOrThrow("WALLET_APP_NAME"),
-  vcIssuerDomain: new URL(getOrElse("VC_ISSUER_DOMAIN", "https://api.dev.sybil.center")),
+  vcIssuerDomain: new URL(getStrOrThrow("VC_ISSUER_DOMAIN")),
 };
 
 function getStrOrThrow(envVar: string): string {
