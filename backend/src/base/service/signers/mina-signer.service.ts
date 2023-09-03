5c6e12003d0bcfc39c01456df0b597e0fb7dbff7
Pave1Pal <pd271828@gmail.com>
2023-09-03 15:31:19 +0300

    chore(backend): add signer manager

--- backend/src/base/service/signers/mina-signer.service.ts
+++ backend/src/base/service/signers/mina-signer.service.ts
@@ -1,21 +1,13 @@
-import { tokens } from "typed-inject";
 import { Config } from "../../../backbone/config.js";
 import { ZkCredential, ZkCredProofed } from "../../types/zkc.credential.js";
 import { TransCredSchema } from "@sybil-center/zkc-preparator";
 import { zkc } from "../../../util/zk-credentials.util.js";
 import { Field, Poseidon, PrivateKey, PublicKey, Signature } from "snarkyjs";
+import { IZkcSigner } from "../../types/zkc.signer.js";
 
-export interface IMinaSigner {
-  identifier: { t: number; k: string };
-  signZkCred(
-    props: Omit<ZkCredential, "isr">,
-    transSchema: TransCredSchema
-  ): ZkCredProofed;
-}
 
-export class MinaSigner implements IMinaSigner {
+export class MinaSigner implements IZkcSigner {
 
-  static inject = tokens("config");
   constructor(
     config: Config,
     private readonly privateKey: PrivateKey = PrivateKey.fromBase58(config.minaPrivateKey),
@@ -29,24 +21,24 @@ export class MinaSigner implements IMinaSigner {
     };
   }
 
-  signZkCred(
+  async signZkCred(
     props: Omit<ZkCredential, "isr">,
     transSchema: TransCredSchema
-  ): ZkCredProofed {
+  ): Promise<ZkCredProofed> {
     const zkCred: ZkCredential = {
       isr: { id: this.identifier },
       ...props
     };
-    const values = zkc.prepare<Field[]>(zkCred, transSchema);
+    let values = zkc.preparator.prepare<Field[]>(zkCred, transSchema)
     const hash = Poseidon.hash(values);
     const signature = Signature.create(this.privateKey, [hash]);
     return {
       ...zkc.sort(zkCred),
       proof: [
         {
-          target: "Mina",
+          target: "mina",
           key: this.publicKey.toBase58(),
-          type: "Mina:Poseidon-BabyJubJub",
+          type: "mina:Poseidon-BabyJubJub",
           transformSchema: transSchema,
           sign: signature.toBase58()
         }
---
+++ backend/src/base/service/signers/zkc.signer-manager.ts
@@ -0,0 +1,49 @@
+import { IZkcSigner } from "../../types/zkc.signer.js";
+import { ZkCredential, ZkCredProofed } from "../../types/zkc.credential.js";
+import { TransCredSchema } from "@sybil-center/zkc-preparator";
+import { Config } from "../../../backbone/config.js";
+import { tokens } from "typed-inject";
+import { ZkcIdAlias } from "../../types/zkc.issuer.js";
+import { MinaSigner } from "./mina-signer.service.js";
+import { zkc } from "../../../util/zk-credentials.util.js";
+import { ClientError } from "../../../backbone/errors.js";
+
+export interface IZkcSignerManager {
+  signer(alias: string): IZkcSigner;
+  signZkCred(
+    alias: string | number,
+    props: Omit<ZkCredential, "isr">,
+    transSchema: TransCredSchema
+  ): Promise<ZkCredProofed>;
+}
+
+export class ZkcSignerManager implements IZkcSignerManager {
+
+  private readonly signers: Record<ZkcIdAlias, IZkcSigner>;
+
+  static inject = tokens("config");
+  constructor(
+    config: Config
+  ) {
+    const minaSigner = new MinaSigner(config);
+    this.signers = {
+      "mina": minaSigner,
+      "1": minaSigner
+    };
+  }
+
+  signer(alias: string): IZkcSigner {
+    const isAlias = zkc.isIdAlias(alias);
+    if (!isAlias) throw new ClientError(`Chain namespace ${alias} is not supported`);
+    return this.signers[alias];
+  }
+
+  signZkCred(
+    alias: string,
+    props: Omit<ZkCredential, "isr">,
+    transSchema: TransCredSchema
+  ): Promise<ZkCredProofed> {
+    const signer = this.signer(alias);
+    return signer.signZkCred(props, transSchema)
+  }
+}
