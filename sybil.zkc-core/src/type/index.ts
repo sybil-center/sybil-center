539eeecc9b5fc963f1124bd6c37f0f52d9416b2e
Pave1Pal <pd271828@gmail.com>
2024-02-17 16:38:19 +0300

    chore: delete zkc useless libs

--- sybil.zkc-core/src/type/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./identifiers.js";
-export * from "./schemas.js";
-export * from "./cred.js";
-export * from "./wallet-provider.js";
--- zkc-core/src/types/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./cred.js";
-export * from "./wallet-provider.js";
-export * from "./issuer.js";
