e4614be8fb220ad233311ab93e19d7b0853e8a1e
Pavel <92925215+pavelgoodpal@users.noreply.github.com>
2024-06-29 17:00:22 +0300

    feat(verifier/hardhat): get eth for creating sybil (#58)

--- verifier/hardhat/contracts/Sybil.sol
+++ verifier/hardhat/contracts/Sybil.sol
@@ -5,28 +5,34 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract Sybil {
 
-  address public owner;
+  address payable public owner;
   mapping(address => bytes20) private sybilMap;
+  uint public requireWei;
 
   event SybilAdd(address subjectId, bytes20 sybilId);
   event ChangeOwner(address newOwner);
+  event ChangeRequireWei(uint newRequireWei);
 
   modifier onlyOwner() {
     require(owner == msg.sender, "only owner");
     _;
   }
 
-  constructor() {
-    owner = msg.sender;
+  constructor(uint _requireWei) {
+    owner = payable(msg.sender);
+    requireWei = _requireWei;
   }
 
-  function setSybilId(bytes20 _sybilId, bytes calldata signature) external {
+  function setSybilId(bytes20 _sybilId, bytes calldata signature) external payable {
+    require(msg.value >= requireWei, "msg.value is not correct");
+    require(sybilMap[msg.sender] == bytes20(0), "address has sybil-id");
     (address recovered, ECDSA.RecoverError err,) = ECDSA.tryRecover(
       keccak256(abi.encodePacked(msg.sender, _sybilId)),
       signature
     );
     require(err == ECDSA.RecoverError.NoError && recovered == owner, "invalid signature");
-    require(sybilMap[msg.sender] == bytes20(0), "address has sybil-id");
+    (bool success,) = owner.call{value: msg.value}("");
+    require(success, "fail to send ETH");
     sybilMap[msg.sender] = _sybilId;
     emit SybilAdd(msg.sender, _sybilId);
   }
@@ -36,8 +42,12 @@ contract Sybil {
   }
 
   function setOwner(address _newOwner) external onlyOwner {
-    owner = _newOwner;
+    owner = payable(_newOwner);
     emit ChangeOwner(_newOwner);
   }
 
+  function setRequireWei(uint _requireWei) external onlyOwner {
+    requireWei = _requireWei;
+    emit ChangeRequireWei(_requireWei);
+  }
 }
