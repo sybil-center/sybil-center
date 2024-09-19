a46d4aeb6b6f350e95c6ae963f19eb7b2ab2fd87
Pave1Pal <pd271828@gmail.com>
2024-09-19 21:24:13 +0300

    fix(verifier): now build

--- verifier/src/types/index.ts
+++ verifier/src/types/index.ts
@@ -24,7 +24,7 @@ export type Proposal = {
   program: JalProgram;
   challenge: {
     message: string;
-    exceptionDifficulty: number;
+    exceptionDifficulty?: number;
   }
   selector: Selector;
   verificationKey?: string;
